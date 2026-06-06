47e474112ff973db3a640b71fb033aa0f35a9c3a
ko-math <sorehakiemasu@gmail.com>
2026-06-06 08:05:29 +0000

    fix file name

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 
 ko-mathサーバーの漢字チャンネルで使用.
+
+
+git add .
+git commit -m "fix file name"
+git push
+
+を実行しないと更新されない！
--- webhook.js
+++ webhook.js
@@ -17,7 +17,7 @@ async function sendKanji(kanji, explanation, imageUrl, articleUrl) {
                         url: imageUrl
                     },
                     footer: {
-                        text: "今日の面白い漢字"
+                        text: "今日の漢字"
                     }
                 }
             ]
@@ -28,12 +28,31 @@ async function sendKanji(kanji, explanation, imageUrl, articleUrl) {
         return;
     }
 
-console.log("送信成功");
+    console.log("送信成功");
 }
 
 sendKanji(
-    "𠄔",
-    "まぼろし",
-    "https://kanji.jitenon.jp/shotai3/27916.gif",
-    "https://kanji.jitenon.jp/kanjiy/27916#google_vignette"
+    "閄",
+    `
+    読み:コク・ワク
+    意味:物陰から急に飛び出して人を驚かせる時に発する声。一部の辞書では
+    「カク・セン」と記されている。
+    大漢和辞典では
+    **「きゅうにとびだしてひとをおどろかせるこゑ 」**
+    という訓読みがあると言われているが、人々の合意はない。
+    また、
+    「わっ-と」
+    という用例も見られる。
+
+    用例.
+    ・**『会談興晤閄雅話』**
+    （かいだんおとしばなしももんがわ・かいだんきょうごももんがわ）
+    -振鷺亭主人作の寛政期の咄本。「ももん」と読ませている。
+    ・**『なんだかとっても！ いいかんじ』**
+    -卑屈Pの初音ミク使用楽曲。サビで「閄」という漢字が
+    「ものかげからきゅうにとびだしてひとをおどろかせるときにはっするこえ」
+    という読み方で使用されている。
+    `,
+    "https://ja.wikipedia.org/wiki/%E9%96%84#/media/%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB:Monhito.gif",
+    "https://ja.wikipedia.org/wiki/%E9%96%84"
 );
